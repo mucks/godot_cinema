6db4da09dfe76ad93ebb9c3198eac760fb4b3346
mucks <mail@mucks.me>
2020-02-07 17:37:33 +0100

    added youtube playback

--- godot_cinema_rs/src/lib.rs
+++ godot_cinema_rs/src/lib.rs
@@ -3,10 +3,56 @@ use opencv::core;
 use opencv::videoio::VideoCapture;
 use opencv::videoio::CAP_ANY;
 
-/// The HelloWorld "class"
+use std::process::{Child, Command};
+use std::sync::mpsc;
+
+pub fn get_youtube_video_url(youtube_url: &str) -> String {
+    let stdout = Command::new("youtube-dl")
+        .args(vec!["--format", "160", "--get-url", youtube_url])
+        .output()
+        .unwrap()
+        .stdout;
+    let url = String::from_utf8(stdout).unwrap();
+    url
+}
+
+fn get_youtube_video(video_url: &str) -> Option<VideoCapture> {
+    let vid = VideoCapture::new_from_file_with_backend(&video_url, CAP_ANY).unwrap();
+    let opened = VideoCapture::is_opened(&vid).unwrap();
+
+    if opened {
+        Some(vid)
+    } else {
+        None
+    }
+}
+
+fn frame_to_img(frame: &mut opencv::core::Mat) -> Image {
+    let size = frame.size().unwrap();
+    let width = size.width;
+    let height = size.height;
+
+    let byte_length = width * height * 3;
+    let mut byte_array = gdnative::ByteArray::new();
+    byte_array.resize(byte_length);
+
+    unsafe {
+        std::ptr::copy(
+            frame.data_mut().unwrap() as *mut _,
+            byte_array.write().as_mut_ptr() as *mut _,
+            byte_length as usize,
+        );
+    }
+    let mut img = Image::new();
+    img.create_from_data(width as i64, height as i64, false, 4, byte_array);
+    img
+}
+
 pub struct Screen {
     pub vid: Option<VideoCapture>,
     pub img: Option<Image>,
+    pub timer: Option<Timer>,
+    pub rx: Option<mpsc::Receiver<VideoCapture>>,
 }
 
 unsafe impl Send for Screen {}
@@ -20,64 +66,44 @@ impl NativeClass for Screen {
     }
 
     fn init(_owner: Self::Base) -> Self {
-        unsafe { Self::_init(_owner) }
+        Self::_init(_owner)
     }
 }
 
-// __One__ `impl` block can have the `#[methods]` attribute, which will generate
-// code to automatically bind any exported methods to Godot.
 #[methods]
 impl Screen {
-    /// The "constructor" of the class.
     fn _init(_owner: Spatial) -> Self {
         Screen {
             vid: None,
             img: None,
+            timer: None,
+            rx: None,
         }
     }
-    // In order to make a method known to Godot, the #[export] attribute has to be used.
-    // In Godot script-classes do not actually inherit the parent class.
-    // Instead they are"attached" to the parent object, called the "owner".
-    // The owner is passed to every single exposed method.
+
     #[export]
-    fn _ready(&mut self, _owner: Spatial) {
-        let mut vid = VideoCapture::new_from_file_with_backend("sample.mp4", CAP_ANY).unwrap();
-        let opened = VideoCapture::is_opened(&vid).unwrap();
-        if opened {
-            self.vid = Some(vid);
-            godot_print!("video is open");
-        } else {
-            self.vid = None;
-            godot_print!("video is closed");
-        }
-        // The `godot_print!` macro works like `println!` but prints to the Godot-editor
-        // output tab as well.
+    fn _ready(&mut self, owner: Spatial) {
+        let (tx, rx) = mpsc::channel();
+        self.rx = Some(rx);
+        std::thread::spawn(move || {
+            let youtube_url = "https://www.youtube.com/watch?v=aqz-KE-bpKQ";
+            let video_url = get_youtube_video_url(youtube_url);
+            let vid = get_youtube_video(&video_url).unwrap();
+            tx.send(vid).unwrap();
+        });
     }
 
     #[export]
     fn _process(&mut self, owner: Spatial, delta: f64) {
+        if let Some(rx) = &self.rx {
+            if let Ok(vid) = rx.try_recv() {
+                self.vid = Some(vid);
+            }
+        }
         if let Some(vid) = &mut self.vid {
             let mut frame = core::Mat::default().unwrap();
             vid.read(&mut frame).unwrap();
-
-            let size = frame.size().unwrap();
-            let width = size.width;
-            let height = size.height;
-
-            let byte_length = width * height * 3;
-            let mut byte_array = gdnative::ByteArray::new();
-            byte_array.resize(byte_length);
-
-            // is causing the crash
-            unsafe {
-                std::ptr::copy(
-                    frame.data_mut().unwrap() as *mut _,
-                    byte_array.write().as_mut_ptr() as *mut _,
-                    byte_length as usize,
-                );
-            }
-            let mut img = Image::new();
-            img.create_from_data(width as i64, height as i64, false, 4, byte_array);
+            let img = frame_to_img(&mut frame);
 
             let mut txt = ImageTexture::new();
             txt.create_from_image(Some(img), 7);
