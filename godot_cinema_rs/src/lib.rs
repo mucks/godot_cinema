bedd6d9b4598e78ad8ec6b58c442130ac487acd7
mucks <mail@mucks.me>
2020-02-09 06:06:33 +0100

    added audio

--- godot_cinema_rs/src/lib.rs
+++ godot_cinema_rs/src/lib.rs
@@ -1,18 +1,49 @@
 use gdnative::*;
+
+use std::fs::File;
+use std::io::Read;
+use std::process::{Child, Command};
+use std::sync::mpsc;
+
 use opencv::core;
 use opencv::videoio::VideoCapture;
 use opencv::videoio::CAP_ANY;
 
-use std::process::{Child, Command};
-use std::sync::mpsc;
-
 pub fn get_youtube_video_url(youtube_url: &str) -> String {
     let stdout = Command::new("youtube-dl")
-        .args(vec!["--format", "160", "--get-url", youtube_url])
+        .args(vec!["--format", "248", "--get-url", youtube_url])
         .output()
         .unwrap()
         .stdout;
     String::from_utf8(stdout).unwrap()
+}
+
+pub fn get_youtube_audio_url(youtube_url: &str) -> String {
+    let stdout = Command::new("youtube-dl")
+        .args(vec!["--format", "249", "--get-url", youtube_url])
+        .output()
+        .unwrap()
+        .stdout;
+    String::from_utf8(stdout).unwrap()
+}
+
+pub fn save_youtube_audio_to_wav(audio_url: &str) -> String {
+    let f_name = "audio.wav";
+
+    godot_print!("ffmpeg started downloading: {}", audio_url);
+
+    let stdout = Command::new("ffmpeg")
+        .args(vec!["-y", "-i", &audio_url.trim(), f_name])
+        .output()
+        .unwrap()
+        .stderr;
+
+    let output = String::from_utf8(stdout).unwrap();
+    godot_print!("ffmpeg output: {}", output);
+
+    godot_print!("ffmpeg works");
+
+    f_name.into()
 }
 
 fn get_youtube_video(video_url: &str) -> Option<VideoCapture> {
@@ -26,7 +57,7 @@ fn get_youtube_video(video_url: &str) -> Option<VideoCapture> {
     }
 }
 
-fn frame_to_img(frame: &mut opencv::core::Mat) -> Image {
+fn frame_to_img(frame: &mut core::Mat) -> Image {
     let size = frame.size().unwrap();
     let width = size.width;
     let height = size.height;
@@ -47,11 +78,18 @@ fn frame_to_img(frame: &mut opencv::core::Mat) -> Image {
     img
 }
 
+pub struct YoutubeInfo {
+    pub video_url: String,
+    pub audio_url: String,
+    pub audio_file_name: String,
+}
+
 pub struct Screen {
     pub vid: Option<VideoCapture>,
-    pub img: Option<Image>,
-    pub timer: Option<Timer>,
-    pub rx: Option<mpsc::Receiver<VideoCapture>>,
+    pub rx: Option<mpsc::Receiver<YoutubeInfo>>,
+    pub frame: core::Mat,
+    pub img_texture: ImageTexture,
+    pub audio_player: Option<AudioStreamPlayer3D>,
 }
 
 unsafe impl Send for Screen {}
@@ -71,18 +109,15 @@ impl NativeClass for Screen {
 
 #[methods]
 impl Screen {
-    fn _init(_owner: Spatial) -> Self {
-        Screen {
+    fn _init(owner: Spatial) -> Self {
+        Self {
             vid: None,
-            img: None,
-            timer: None,
+            frame: core::Mat::default().unwrap(),
             rx: None,
+            audio_player: None,
+            // needs to be initialized here otherwise it will cause major memory leak
+            img_texture: ImageTexture::new(),
         }
-    }
-
-    #[export]
-    fn _on_youtube_url_entry_text_entered(&mut self, owner: Spatial, youtube_url: String) {
-        self.add_youtube_video(youtube_url)
     }
 
     fn add_youtube_video(&mut self, youtube_url: String) {
@@ -91,53 +126,102 @@ impl Screen {
 
         std::thread::spawn(move || {
             let video_url = get_youtube_video_url(youtube_url.as_str());
-            let vid = get_youtube_video(&video_url).unwrap();
-            tx.send(vid).unwrap();
+            let audio_url = get_youtube_audio_url(youtube_url.as_str());
+            let audio_file_name = save_youtube_audio_to_wav(&audio_url);
+
+            let youtube_info = YoutubeInfo {
+                video_url,
+                audio_url,
+                audio_file_name,
+            };
+            tx.send(youtube_info).unwrap();
         });
     }
 
     #[export]
-    fn _ready(&mut self, owner: Spatial) {}
+    fn _on_youtube_url_entry_text_entered(&mut self, owner: Spatial, youtube_url: String) {
+        self.add_youtube_video(youtube_url);
+    }
+
+    #[export]
+    fn _ready(&mut self, owner: Spatial) {
+        self.audio_player = unsafe {
+            owner
+                .get_node(NodePath::from_str("Audio"))
+                .unwrap()
+                .cast::<AudioStreamPlayer3D>()
+        };
+        let video = "https://www.youtube.com/watch?v=OfIQW6s1-ew";
+
+        //self.add_youtube_video(video.into());
+    }
 
     #[export]
     fn _process(&mut self, owner: Spatial, delta: f64) {
         if let Some(rx) = &self.rx {
-            if let Ok(vid) = rx.try_recv() {
-                self.vid = Some(vid);
+            if let Ok(youtube_info) = rx.try_recv() {
+                let mut f = File::open(&youtube_info.audio_file_name).unwrap();
+                let mut bytes: Vec<u8> = Vec::new();
+                f.read_to_end(&mut bytes).unwrap();
+
+                let byte_length = bytes.len();
+
+                let mut byte_array = gdnative::ByteArray::new();
+                byte_array.resize(byte_length as i32);
+
+                unsafe {
+                    std::ptr::copy(
+                        bytes.as_mut_ptr() as *mut _,
+                        byte_array.write().as_mut_ptr() as *mut _,
+                        byte_length,
+                    );
+                }
+
+                let mut audio_sample = AudioStreamSample::new();
+                audio_sample.set_data(byte_array);
+
+                // sets audio to 16bit
+                audio_sample.set_format(1);
+
+                // very funny audio results without this
+                audio_sample.set_mix_rate(48000);
+                audio_sample.set_stereo(true);
+
+                unsafe {
+                    if let Some(audio_player) = &mut self.audio_player {
+                        audio_player.set_stream(audio_sample.cast::<AudioStream>());
+                        audio_player.play(0.0);
+                    }
+                }
+
+                self.vid = get_youtube_video(&youtube_info.video_url);
             }
         }
-        if let Some(vid) = &mut self.vid {
-            let mut frame = core::Mat::default().unwrap();
-            vid.read(&mut frame).unwrap();
 
-            let img = frame_to_img(&mut frame);
-            unsafe { self.apply_img_to_texture(owner, img) }
+        if let Some(vid) = &mut self.vid {
+            vid.read(&mut self.frame).unwrap();
+            let img = frame_to_img(&mut self.frame);
+            self.apply_img_to_texture(owner, img);
         }
     }
 
-    unsafe fn apply_img_to_texture(&mut self, owner: Spatial, img: Image) {
-        let mut txt = ImageTexture::new();
-        txt.create_from_image(Some(img), 7);
-
+    fn apply_img_to_texture(&mut self, owner: Spatial, img: Image) {
+        self.img_texture.create_from_image(Some(img), 7);
         let path = NodePath::from_str("Screen/CollisionShape/MeshInstance");
 
-        let mesh = owner
-            .get_node(path)
-            .unwrap()
-            .cast::<MeshInstance>()
-            .unwrap();
-        if let Some(mat) = mesh.get_surface_material(0) {
-            let mut mat = mat.cast::<SpatialMaterial>().unwrap();
-            mat.set_texture(0, txt.cast::<Texture>());
-        }
-    }
+        let mesh = unsafe {
+            owner
+                .get_node(path)
+                .unwrap()
+                .cast::<MeshInstance>()
+                .unwrap()
+        };
 
-    #[export]
-    fn get_img(&mut self, _owner: Spatial) -> Image {
-        if let Some(img) = &self.img {
-            img.new_ref()
-        } else {
-            Image::new()
+        let option_mat = unsafe { mesh.get_surface_material(0) };
+        if let Some(mat) = option_mat {
+            let mut spatial_mat = mat.cast::<SpatialMaterial>().unwrap();
+            let texture = self.img_texture.to_texture();
+            spatial_mat.set_texture(0, Some(texture));
         }
     }
 }
